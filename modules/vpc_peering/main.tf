6047b6de174510fd136d7c73aa7cea65d28e927f
shivanibhat008 <shivani.bhat008@gmail.com>
2026-06-23 00:10:36 +0100

    Update main.tf

--- modules/vpc_peering/main.tf
+++ modules/vpc_peering/main.tf
@@ -1,45 +1,28 @@
-terraform {
-  required_providers {
-    aws = {
-      source                = "hashicorp/aws"
-      configuration_aliases = [aws.hub, aws.spoke]
-    }
-  }
-}
+# ==============================================================================
+# ENABLE CROSS-VPC DNS RESOLUTION
+# ==============================================================================
 
-resource "aws_vpc_peering_connection" "hub_to_spoke" {
-  provider      = aws.hub
-  vpc_id        = var.hub_vpc_id
-  peer_vpc_id   = var.spoke_vpc_id
-  peer_region   = var.spoke_region
-  peer_owner_id = var.spoke_account_id
-  auto_accept   = false
-
-  tags = {
-    Name = var.peering_name
-  }
-}
-
-resource "aws_vpc_peering_connection_accepter" "spoke_accepts" {
-  provider                  = aws.spoke
-  vpc_peering_connection_id = aws_vpc_peering_connection.hub_to_spoke.id
-  auto_accept               = true
-
-  tags = {
-    Name = "${var.peering_name}-accepter"
-  }
-}
-
-resource "aws_route" "hub_to_spoke_route" {
+# 1. Modify the Requester Options (Hub)
+resource "aws_vpc_peering_connection_options" "hub_dns_resolution" {
   provider                  = aws.hub
-  route_table_id            = var.hub_route_table_id
-  destination_cidr_block    = var.spoke_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.hub_to_spoke.id
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  # Options can only be set after the connection is active
+  depends_on = [aws_vpc_peering_connection_accepter.spoke_accepts_hub]
 }
 
-resource "aws_route" "spoke_to_hub_route" {
+# 2. Modify the Accepter Options (Spoke)
+resource "aws_vpc_peering_connection_options" "spoke_dns_resolution" {
   provider                  = aws.spoke
-  route_table_id            = var.spoke_route_table_id
-  destination_cidr_block    = var.hub_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.hub_to_spoke.id
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  depends_on = [aws_vpc_peering_connection_accepter.spoke_accepts_hub]
 }
