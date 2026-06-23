0d2db43aacc45e6e1113e4da2efaa1eec8c2b15e
shivanibhat008 <shivani.bhat008@gmail.com>
2026-06-23 02:53:59 +0100

    Update main.tf

--- modules/vpc_peering/main.tf
+++ modules/vpc_peering/main.tf
@@ -1,28 +1,46 @@
-# ==============================================================================
-# ENABLE CROSS-VPC DNS RESOLUTION
-# ==============================================================================
+# 1. The Connection (Requester)
+resource "aws_vpc_peering_connection" "mesh" {
+  vpc_id      = var.hub_vpc_id
+  peer_vpc_id = var.spoke_vpc_id
+  peer_region = var.spoke_region
 
-# 1. Modify the Requester Options (Hub)
-resource "aws_vpc_peering_connection_options" "hub_dns_resolution" {
-  provider                  = aws.hub
-  vpc_peering_connection_id = aws_vpc_peering_connection.hub_to_spoke.id
-
-  requester {
-    allow_remote_vpc_dns_resolution = true
+  tags = {
+    Name = "${var.project_name}-${var.environment}-transit-mesh"
   }
-
-  # Options can only be set after the connection is active
-  depends_on = [aws_vpc_peering_connection_accepter.spoke_accepts_hub]
 }
 
-# 2. Modify the Accepter Options (Spoke)
-resource "aws_vpc_peering_connection_options" "spoke_dns_resolution" {
-  provider                  = aws.spoke
-  vpc_peering_connection_id = aws_vpc_peering_connection.hub_to_spoke.id
+# 2. The Connection (Accepter - requires aliased provider in root)
+resource "aws_vpc_peering_connection_accepter" "mesh_accepter" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.mesh.id
+  auto_accept               = true
 
-  accepter {
-    allow_remote_vpc_dns_resolution = true
+  tags = {
+    Name = "${var.project_name}-${var.environment}-transit-accepter"
   }
+}
 
-  depends_on = [aws_vpc_peering_connection_accepter.spoke_accepts_hub]
+# 3. Cross-VPC DNS Resolution Options
+resource "aws_vpc_peering_connection_options" "hub_dns" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.mesh.id
+  requester { allow_remote_vpc_dns_resolution = true }
+  depends_on = [aws_vpc_peering_connection_accepter.mesh_accepter]
+}
+
+resource "aws_vpc_peering_connection_options" "spoke_dns" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.mesh.id
+  accepter { allow_remote_vpc_dns_resolution = true }
+  depends_on = [aws_vpc_peering_connection_accepter.mesh_accepter]
+}
+
+# 4. Layer 3 Route Injections
+resource "aws_route" "hub_to_spoke" {
+  route_table_id            = var.hub_route_table_id
+  destination_cidr_block    = var.spoke_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.mesh.id
+}
+
+resource "aws_route" "spoke_to_hub" {
+  route_table_id            = var.spoke_route_table_id
+  destination_cidr_block    = var.hub_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.mesh.id
 }
