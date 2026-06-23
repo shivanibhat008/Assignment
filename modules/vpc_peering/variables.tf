4d8e67a613dd969e276c983157075063e843ad2a
shivanibhat008 <shivani.bhat008@gmail.com>
2026-06-23 03:01:47 +0100

    Update variables.tf

--- modules/vpc_peering/variables.tf
+++ modules/vpc_peering/variables.tf
@@ -1,44 +1,44 @@
-variable "peering_name" {
-  description = "Name tag for the VPC Peering Connection"
+variable "project_name" {
+  description = "The overarching name of the project."
+  type        = string
+}
+
+variable "environment" {
+  description = "The deployment environment."
   type        = string
 }
 
 variable "hub_vpc_id" {
-  description = "The VPC ID of the Central Hub (Requester)"
+  description = "The VPC ID of the Central Hub (Requester)."
   type        = string
 }
 
 variable "hub_vpc_cidr" {
-  description = "The CIDR block of the Central Hub for Spoke routing"
+  description = "The CIDR block of the Central Hub, used to inject return routes into the Spoke."
   type        = string
 }
 
 variable "hub_route_table_id" {
-  description = "The ID of the Hub's private route table"
+  description = "The ID of the Hub's private route table to inject the peering route."
   type        = string
 }
 
 variable "spoke_vpc_id" {
-  description = "The VPC ID of the Regional Spoke (Accepter)"
+  description = "The VPC ID of the Regional Spoke (Accepter)."
   type        = string
 }
 
 variable "spoke_vpc_cidr" {
-  description = "The CIDR block of the Spoke for Hub routing"
+  description = "The CIDR block of the Regional Spoke, used to inject outbound routes into the Hub."
   type        = string
 }
 
 variable "spoke_route_table_id" {
-  description = "The ID of the Spoke's private route table"
+  description = "The ID of the Spoke's private route table to inject the peering route."
   type        = string
 }
 
 variable "spoke_region" {
-  description = "The AWS region of the Spoke VPC"
-  type        = string
-}
-
-variable "spoke_account_id" {
-  description = "The AWS Account ID of the Spoke VPC owner"
+  description = "The AWS Region of the Spoke VPC, required by the peering connection requester."
   type        = string
 }
