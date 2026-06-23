4c484b008d012e44939c8a7d677cd7e3a96e2df4
shivanibhat008 <shivani.bhat008@gmail.com>
2026-06-23 03:00:08 +0100

    Update outputs.tf

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,19 +1,14 @@
 output "vpc_id" {
-  description = "The ID of the VPC"
+  description = "The ID of the provisioned VPC."
   value       = aws_vpc.this.id
 }
 
 output "private_route_table_id" {
-  description = "The ID of the private route table for peering and endpoint attachments"
+  description = "The ID of the private route table used for endpoint and peering injections."
   value       = aws_route_table.private.id
 }
 
-output "vpc_cidr" {
-  description = "The CIDR block of the VPC for security group whitelisting"
-  value       = aws_vpc.this.cidr_block
-}
-
-output "private_subnet_id" {
-  description = "The ID of the private subnet"
-  value       = aws_subnet.private.id
+output "dynamodb_gateway_endpoint_id" {
+  description = "The ID of the VPC Gateway Endpoint for DynamoDB."
+  value       = aws_vpc_endpoint.dynamodb.id
 }
