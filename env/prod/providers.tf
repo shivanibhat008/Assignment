005574549869fb236eda51d9cd80984c249c939d
shivanibhat008 <shivani.bhat008@gmail.com>
2026-06-28 16:00:40 +0100

    Update providers.tf

--- env/prod/providers.tf
+++ env/prod/providers.tf
@@ -8,9 +8,15 @@ terraform {
 provider "aws" {
   alias  = "hub"
   region = var.hub_region
+assume_role {
+    role_arn = var.hub_role_arn
+  }
 }
 
 provider "aws" {
   alias  = "spoke"
   region = var.spoke_region
+assume_role {
+    role_arn = var.spoke_role_arn
+  }
 }
