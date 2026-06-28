2b47403316a50d4370c308296e939d254ad54cf2
shivanibhat008 <shivani.bhat008@gmail.com>
2026-06-28 20:35:48 +0100

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "hub_lambda_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.spoke_vpc_cidr]
   }
 }
 
