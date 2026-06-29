1803e40239afafcd6eb4ae9b3ee4750937b827bb
shivanibhat008 <shivani.bhat008@gmail.com>
2026-06-29 17:03:10 +0100

    Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -10,16 +10,18 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "private" {
-  count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index]
+
+  count = var.private_subnet_count
+  vpc_id = aws_vpc.this.id
+  cidr_block = cidrsubnet(var.vpc_cidr,8,count.index)
+
+    availability_zone =
+    data.aws_availability_zones.available.names[count.index]
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-private-subnet-${count.index + 1}"
+    Name = "${var.project_name}-${var.environment}-private-${count.index + 1}"
   }
 }
-
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
